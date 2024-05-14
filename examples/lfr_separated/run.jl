83a77967bcfc55fbb54d395a6129df4874ccb9e0
Gaspard Jankowiak <gaspard.jankowiak@uni-graz.at>
2024-05-14 17:30:37 +0200

    update LFR example

--- examples/lfr_separated/run.jl
+++ examples/lfr_separated/run.jl
@@ -1,5 +1,6 @@
 using Distributed
 @everywhere import OpiForm
+@everywhere import DelimitedFiles
 # import OpiForm
 
 function main()
@@ -33,7 +34,6 @@ function main()
       store_g=false,
       init_method_omega=:from_lfr,
       init_method_adj_matrix=:from_lfr,
-      f_init_func=OpiForm.Params.build_f_init_func_beta(; n_communities=3, σ²=β_σ², expectation_bounds=expectation_bounds),
       init_method_f=:from_f_init,
       init_method_g=:from_kde_adj_matrix,
       init_lfr_args=(k_mean, k_max),
@@ -47,7 +47,7 @@ function main()
       init_lfr_target_n_communities=3
     ))
 
-    prefix = "LFR_separated/N_micro=$(N_micro),N_mfl=$(N_mfl)/σ²=$(β_σ²)/μ=$μ"
+    prefix = "LFR_separated_2/N_micro=$(N_micro),N_mfl=$(N_mfl)/σ²=$(β_σ²)/μ=$μ"
 
     # Run the micro model
     store_dir_micro = "results/$(prefix)/micro-$(run_id)"
@@ -63,11 +63,14 @@ function main()
       end
       return
     end
+    c_ids = vec(map(Int, DelimitedFiles.readdlm(joinpath(store_dir_micro, "c_ids.csv"))))
+    c_expectations = vec(DelimitedFiles.readdlm(joinpath(store_dir_micro, "c_expectations.csv")))
 
     # Run the meanfield model using the initial data and graph of the micro model (with KDE)
     store_dir_mfl = "results/$(prefix)/meanfield-$(run_id)"
     params_lLF = merge(params, (
       flux=:lLF, f_dependent_g=false,
+      f_init_func=OpiForm.Params.build_f_init_func_beta_weighted(; communities=c_ids, community_expectations=c_expectations, σ²=β_σ²),
       init_method_omega=:from_file,
       init_method_adj_matrix=:from_file,
       init_micro_filename=joinpath(store_dir_micro, "data.hdf5"),
@@ -85,8 +88,8 @@ function main()
     end
   end
 
-  µs = [1e-3; 5e-3; 1e-2; range(0.05, 1.0, 7)]
-  β_σ²s = [1e-3, 2e-3, 4e-3, 1.2e-2]
+  µs = [1e-3; 5e-3; 1e-2; 5e-1; range(0.05, 1.0, 7)]
+  β_σ²s = [1e-3, 4e-3, 1.2e-2]
 
   n_runs = 5
 

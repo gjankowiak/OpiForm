ccb1ad962dd5cc20c66fd5e9f6c7d74319e88bf7
Gaspard Jankowiak <gaspard.jankowiak@uni-graz.at>
2025-05-07 15:17:26 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ set -e
 mkdir 'OpiForm'
 cd 'OpiForm'
 JULIA_PKG_DEVDIR="$PWD/dev" julia --project=. -e 'import Pkg; Pkg.develop(url="https://github.com/gjankowiak/OpiForm"); Pkg.resolve(); Pkg.instantiate()'
-cp dev/OpiForm/examples/example.jl .
 
 echo 'You should be ready to go! Try:'
 echo 'cd OpiFrom'
-echo 'julia --project=. example.jl'
+echo 'julia --project=. -e \'include("examples/paper/beta_2_with_var/run_monoproc.jl")\''

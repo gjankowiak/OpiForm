c29a8b6597540377bc7b41207af915432aaad9b6
Gaspard Jankowiak <gaspard.jankowiak@uni-graz.at>
2026-01-29 12:00:00 +0100

    Specify 'multigroup' revision for OpiForm package
    Updated the Julia package development command to specify the 'multigroup' revision.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 
 mkdir 'OpiForm'
 cd 'OpiForm'
-JULIA_PKG_DEVDIR="$PWD/dev" julia --project=. -e 'import Pkg; Pkg.develop(url="https://github.com/gjankowiak/OpiForm"); Pkg.resolve(); Pkg.instantiate()'
+JULIA_PKG_DEVDIR="$PWD/dev" julia --project=. -e 'import Pkg; Pkg.develop(url="https://github.com/gjankowiak/OpiForm", rev="multigroup"); Pkg.resolve(); Pkg.instantiate()'
 
 echo 'You should be ready to go! Try:'
 echo 'cd OpiFrom'
